bbfae01ef10c67974151e5beeeef35ff569f77eb
James Bodkin <JBodkin@users.noreply.github.com>
2014-10-13 17:23:08 +0100

    Add jquery-ondemand

---
+++ src/jquery-ondemand.js
@@ -0,0 +1,38 @@
+/*
+ * OnDemand - Attach javascript events on dynamic content - Inspired by Drupal Behaviours
+ * Copyright (C) 2014 James Bodkin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+;(function($, window, document, undefined) {
+
+    'use strict';
+
+    window.OnDemand = {};
+
+    $.OnDemandAttach = function(context) {
+        context = context || document;
+
+        $.each(OnDemand, function() {
+            if ($.isFunction(this.attach)) {
+                this.attach(context);
+            }
+        });
+    };
+
+    $(document).ready(function() {
+        $.OnDemandAttach();
+    });
+
+}(jQuery, window, document));
